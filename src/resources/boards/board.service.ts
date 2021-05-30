c8c6b89dbda890be4e9d7334cd659b65e692a3ae
Fedor Odintsov <fadmin@list.ru>
2021-05-30 21:01:37 +0300

    refactor: translate Board to TS

---
+++ src/resources/boards/board.service.ts
@@ -0,0 +1,14 @@
+import * as boardsRepo from './board.memory.repository';
+import Board from './board.model';
+
+const getAll = (): Promise<Board[]> => boardsRepo.getAll();
+
+const create = (board: Board): Promise<Board> => boardsRepo.create(board);
+
+const getById = (id: string): Promise<Board|undefined> => boardsRepo.getById(id);
+
+const update = (id: string, board: Board): Promise<Board|undefined> => boardsRepo.update(id, board);
+
+const remove = (id: string): Promise<boolean> => boardsRepo.remove(id);
+
+export { getAll, create, getById, update, remove };
