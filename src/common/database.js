58d4a0e00d43ff9046eba9d2d60b3ddadf5be174
Fedor Odintsov <fadmin@list.ru>
2021-05-30 07:42:40 +0300

    feat: add jsDoc comments to Boards

--- src/common/database.js
+++ src/common/database.js
@@ -8,14 +8,14 @@ let tasks = [new Task()];
 
 // Users
 /**
- * Returns all user's records
+ * Returns all users records
  * @returns {User[]} Return array of all users
  */
 const getAllUsers = () => users;
 
 /**
  * Creates new user and returns it
- * @param user object with name, login, password of new user
+ * @param user {object} name, login, password of new user
  * @returns {User} user object
  */
 const createUser = (user) => {
@@ -27,15 +27,15 @@ const createUser = (user) => {
 /**
  * Returns user specified by Id
  * @param id {string} - id of user
- * @returns {User} user object
+ * @returns {User|[]} User object or empty array if found nothing
  */
 const getUserById = (id) => users.filter((user) => user.id === id)[0];
 
 /**
  * Updates user data
  * @param id {string} - id of user
- * @param userData - new user data
- * @returns {User|undefined} returns updated User or undefined if not found
+ * @param userData {object} - new user data
+ * @returns {User|undefined} returns updated User or undefined if found nothing
  */
 const updateUser = (id, userData) => {
   if (!users.some((user) => user.id === id)) {
@@ -48,8 +48,8 @@ const updateUser = (id, userData) => {
 
 /**
  * Removes user by Id
- * @param id {string} - id of user
- * @returns {boolean} - returns True if success and False if Id was not found
+ * @param id {string} id of user
+ * @returns {boolean} returns True if success and False if Id was not found
  */
 const removeUser = (id) => {
   if (!users.some((user) => user.id === id)) {
@@ -67,16 +67,36 @@ const removeUser = (id) => {
 };
 
 // Boards
+/**
+ * Returns all boards records
+ * @returns {Board[]} return array of Board objects
+ */
 const getAllBoards = () => boards;
 
+/**
+ * Creates new board
+ * @param board {object} - object with title and columns
+ * @returns {Board}
+ */
 const createBoard = (board) => {
   const newBoard = new Board(board);
   boards.push(newBoard);
   return newBoard;
 };
 
+/**
+ * Returns board specified by Id
+ * @param id {string} id of board
+ * @returns {Board|[]} returns Board object or empty array if found nothing
+ */
 const getBoardById = (id) => boards.filter((board) => board.id === id)[0];
 
+/**
+ * Updates board
+ * @param id {string} id of board
+ * @param boardData {object} new title and columns
+ * @returns {Board|undefined} returns updated Board or undefined if found nothing
+ */
 const updateBoard = (id, boardData) => {
   if (!boards.some((board) => board.id === id)) {
     return undefined;
@@ -86,6 +106,11 @@ const updateBoard = (id, boardData) => {
   return { id, ...boardData };
 };
 
+/**
+ * Removes Board and it's tasks records
+ * @param id {string} id of board
+ * @returns {boolean} returns True if success and False if Id was not found
+ */
 const removeBoard = (id) => {
   if (!boards.some((board) => board.id === id)) {
     return false;
