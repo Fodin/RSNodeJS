4f05be7bd0b0b5233789af5b1fb99452369eb753
Fedor Odintsov <fadmin@list.ru>
2021-06-06 18:46:36 +0300

    feat: use http-status-codes

--- src/resources/tasks/task.router.ts
+++ src/resources/tasks/task.router.ts
@@ -1,43 +1,44 @@
 import Express, { Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import * as tasksService from './task.service.js';
 
 const router = Router({ mergeParams: true });
 
 router.route('/').get(async (_req, res) => {
   const tasks = await tasksService.getAll();
-  res.json(tasks);
+  res.status(StatusCodes.OK).json(tasks);
 });
 
 router.route('/').post(async (req: Express.Request, res) => {
   const { boardId } = req.params;
   const task = await tasksService.create({ ...req.body, boardId} );
-  res.status(201).json(task);
+  res.status(StatusCodes.CREATED).json(task);
 });
 
 router.route('/:id').get(async (req, res) => {
   const task = await tasksService.getById(req.params.id);
   if (task) {
-    res.status(200).json(task);
+    res.status(StatusCodes.OK).json(task);
   } else {
-    res.status(404).send('Task not found');
+    res.status(StatusCodes.NOT_FOUND).send('Task not found');
   }
 });
 
 router.route('/:id').put(async (req, res) => {
   const task = await tasksService.update(req.params.id, req.body);
   if (task) {
-    res.status(200).json(task);
+    res.status(StatusCodes.OK).json(task);
   } else {
-    res.status(400).send('Bad request');
+    res.status(StatusCodes.BAD_REQUEST).send('Bad request');
   }
 });
 
 router.route('/:id').delete(async (req, res) => {
   const task = await tasksService.remove(req.params.id);
   if (task) {
-    res.status(204).send('The task has been deleted');
+    res.status(StatusCodes.NO_CONTENT).send('The task has been deleted');
   } else {
-    res.status(201).send('Bad request');
+    res.status(StatusCodes.BAD_REQUEST).send('Bad request');
   }
 });
 
--- src/resources/users/user.router.ts
+++ src/resources/users/user.router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import User from './user.model.js';
 import * as usersService from './user.service.js';
 
@@ -6,38 +7,38 @@ const router = Router();
 
 router.route('/').get(async (_req, res) => {
   const users = await usersService.getAll();
-  res.json(users.map(User.toResponse));
+  res.status(StatusCodes.OK).json(users.map(User.toResponse));
 });
 
 router.route('/').post(async (req, res) => {
   const user = await usersService.create(req.body);
-  res.status(201).json(User.toResponse(user));
+  res.status(StatusCodes.CREATED).json(User.toResponse(user));
 });
 
 router.route('/:id').get(async (req, res) => {
   const user = await usersService.getById(req.params.id);
   if (user) {
-    res.status(200).json(User.toResponse(user));
+    res.status(StatusCodes.OK).json(User.toResponse(user));
   } else {
-    res.status(404).send('User not found');
+    res.status(StatusCodes.NOT_FOUND).send('User not found');
   }
 });
 
 router.route('/:id').put(async (req, res) => {
   const user = await usersService.update(req.params.id, req.body);
   if (user) {
-    res.status(200).json(User.toResponse(user));
+    res.status(StatusCodes.OK).json(User.toResponse(user));
   } else {
-    res.status(400).send('Bad request');
+    res.status(StatusCodes.BAD_REQUEST).send('Bad request');
   }
 });
 
 router.route('/:id').delete(async (req, res) => {
   const user = await usersService.remove(req.params.id);
   if (user) {
-    res.status(204).send('The user has been deleted');
+    res.status(StatusCodes.NO_CONTENT).send('The user has been deleted');
   } else {
-    res.status(201).send('Bad request');
+    res.status(StatusCodes.BAD_REQUEST).send('Bad request');
   }
 });
 
